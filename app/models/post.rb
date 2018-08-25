353fda7cf8135802d78fe15677f0cf6bf9dac3b7
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2018-08-25 16:08:07 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250 }
+  validates :summary, length: {maximum: 250 }
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validate :title_does_not_contain_specified_words
+
+
+  def title_does_not_contain_specified_words 
+    if !title.nil? && !["Won't Believe","Secret","Top","Guess"].any? {|word| title.include?(word)}
+      errors.add(:title, "Does not include the clickbait words.")
+    end 
+  end 
 end
